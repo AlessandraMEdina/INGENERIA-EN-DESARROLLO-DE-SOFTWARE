b7ab16bcae1154074ff5d39a01cfa6eeef8e0904
Alessandra Medina Soto <131724781+AlessandraMEdina@users.noreply.github.com>
2023-12-03 20:06:31 -0700

    Operaciones Básicas en C++
    Suma, Resta, Multiplicación y División

---
+++ Actividad2_AlessandraMedinaSoto-LP1.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero1;
+    int numero2;
+    cout << "escribe el primero numero: ";
+    cin >> numero1;
+    cout << "escribe el segundo numero: ";
+    cin >> numero2;
+    cout << "la suma es: " << numero1 + numero2;
+    cout << endl;
+    cout << "la resta es: " << numero1 - numero2;
+    cout << endl;
+    cout << "la multiplicacion es: " << numero1 * numero2;
+    cout << endl;
+    cout << "la division es: " << numero1 / numero2;
+    cout << endl;
+    
+    
+    return 0;
+}
